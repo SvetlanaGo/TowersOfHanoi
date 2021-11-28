a89476b6dc73df0b3370f6fd0d6676ebde99318a
SvetlanaGo <Svetlanag_07@mail.ru>
2021-11-28 12:04:36 +0300

    Add project files.

---
+++ HanoiTowers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+#pragma warning disable S2234
+
+namespace TowersOfHanoi
+{
+    /// <summary>
+    /// Static class TowerOfHanoi.
+    /// </summary>
+    public static class HanoiTowers
+    {
+        /// <summary>
+        /// Get count of moves to move all disks from the first tower to the third.
+        /// </summary>
+        /// <param name="disks">Count of disks.</param>
+        /// <returns>All moves.</returns>
+        public static IEnumerable<(Tower, Tower)> GetMoves(int disks)
+        {
+            //2 ^ п - 1 steps.
+            if (disks < 1 || disks > 30)
+            {
+                throw new ArgumentException("Count of disks should be more than 0 and less or equal 30.", nameof(disks));
+            }
+
+            return MoveDiscs(disks, Tower.From, Tower.To, Tower.Buffer);
+        }
+
+        private static IEnumerable<(Tower, Tower)> MoveDiscs(int countDiscs, Tower from, Tower to, Tower buffer)
+        {
+            if (countDiscs > 0)
+            {
+                foreach (var move in MoveDiscs(countDiscs - 1, from, buffer, to))
+                {
+                    yield return move;
+                }
+                    
+                yield return (from, to);
+                foreach (var move in MoveDiscs(countDiscs - 1, buffer, to, from))
+                {
+                    yield return move;
+                }
+            }
+        }
+    }
+}
---
+++ Tower.cs
@@ -0,0 +1,23 @@
+﻿namespace TowersOfHanoi
+{
+    /// <summary>
+    /// Represents a tower of Hanoi.
+    /// </summary>
+    public enum Tower : byte
+    {
+        /// <summary>
+        /// First tower.
+        /// </summary>
+        From = 1,
+
+        /// <summary>
+        /// Second tower.
+        /// </summary>
+        Buffer,
+
+        /// <summary>
+        /// Third tower.
+        /// </summary>
+        To,
+    }
+}
